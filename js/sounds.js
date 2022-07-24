c81072e24cc7fa142ee8764d323202c95557c7b6
César Amorim <84666621+amorimcesar@users.noreply.github.com>
2022-07-24 09:39:20 -0300

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -2,10 +2,10 @@ export function Sounds() {
 
   const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
   const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-  const bgAudioFlorest = new Audio('../projeto05/assets/Floresta.wav')
-  const bgAudioRain = new Audio("../projeto05/assets/Chuva.wav")
-  const bgAudioCoffe = new Audio("../projeto05/assets/Cafeteria.wav")
-  const bgAudioFirePlace = new Audio("../projeto05/assets/Lareira.wav")
+  const bgAudioFlorest = new Audio('../assets/Floresta.wav')
+  const bgAudioRain = new Audio("../assets/Chuva.wav")
+  const bgAudioCoffe = new Audio("../assets/Cafeteria.wav")
+  const bgAudioFirePlace = new Audio("../assets/Lareira.wav")
 
   bgAudioFlorest.loop = true;
   bgAudioRain.loop = true;
